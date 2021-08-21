02f696abb1ea0f132d685feecde6fad28bc5b86c
Ahzam Shahnil <62467164+ahzam-shahnil@users.noreply.github.com>
2021-08-21 11:42:32 +0500

    refractoring and fixing bugs

---
+++ lib/controller/ThemeController.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../service/SharedStorage.dart';
+
+class ThemeController extends GetxController {
+  var themeValue = 0.obs;
+  @override
+  void onInit() {
+    getSavedTheme();
+    super.onInit();
+  }
+
+  void handleRadioValueChange(var value) {
+    themeValue.value = value!;
+    SharedStorage.saveSelectedTheme(value: value);
+    Get.changeThemeMode(
+      value == 0
+          ? ThemeMode.light
+          : value == 1
+              ? ThemeMode.dark
+              : ThemeMode.system,
+    );
+    Get.back();
+  }
+
+  Future<void> getSavedTheme() async =>
+      themeValue.value = (await SharedStorage.getCurrentTheme()) ?? 0;
+}
---
+++ lib/widgets/ThemeTile.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controller/ThemeController.dart';
+
+class ThemeTile extends StatelessWidget {
+  const ThemeTile({
+    Key? key,
+    required this.themeController,
+    required this.value,
+    required this.title,
+  }) : super(key: key);
+
+  final ThemeController themeController;
+  final int value;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => ListTile(
+        contentPadding: EdgeInsets.symmetric(horizontal: 6),
+        minLeadingWidth: 20,
+        onTap: () => themeController.handleRadioValueChange(value),
+        leading: Radio(
+          value: value,
+          groupValue: themeController.themeValue.value,
+          onChanged: (value) => themeController.handleRadioValueChange(value),
+        ),
+        title: Text(
+          title,
+        ),
+      ),
+    );
+  }
+}
